5854b46adee6ec44e4adc866de4aade004bacc89
behuamuh <sven1980@list.ru>
2018-06-01 08:23:22 +0300

    Added task3

---
+++ task3.rb
@@ -0,0 +1,9 @@
+# Task 3
+str = ''
+for i in 0 ... ARGV.length
+  str = str + ARGV[i]
+end
+d = str.scan(/\d/).join
+g = str.scan(/[aeiouyAEIOUYаеёиоуыэюяАЕЁИОУЫЭЮЯ]/).join
+s = str.scan(/[qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNMйцкнгшщзхъфвпрлджчсмтьбЙЦКНГШЩЗХЪФВПРЛДЖЧСМТЬБ]/).join
+puts g + s + d
