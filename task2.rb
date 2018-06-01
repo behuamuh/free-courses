f9bd154e8930a27d257fc168d4b24d79ed428072
behuamuh <sven1980@list.ru>
2018-06-01 07:44:12 +0300

    Added task2

---
+++ task2.rb
@@ -0,0 +1,18 @@
+# Task 2
+num = ARGV[0].to_i
+n = num.abs
+f0 = 0
+f1 = 1
+if n==1 || n ==0
+  puts n
+  exit
+end
+for i in 2 .. n
+  fi = f0 + f1
+  f0 = f1
+  f1 = fi
+end
+if num < 0 && n % 2 == 0
+  fi = -fi
+end
+puts fi
