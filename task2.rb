8431764548d925f9a0e3b62c837d96fe0ef0fbe4
behuamuh <sven1980@list.ru>
2018-06-04 14:17:57 +0300

    Update task2.rb

--- task2.rb
+++ task2.rb
@@ -1,18 +1,22 @@
 # Task 2
+def fibo(num)
+  n = num.abs
+  f0 = 0
+  f1 = 1
+  if n==1 || n ==0
+    return n
+    exit
+  end
+  for i in 2 .. n
+    fi = f0 + f1
+    f0 = f1
+    f1 = fi
+  end
+  if num < 0 && n % 2 == 0
+    fi = -fi
+  end
+  return fi
+end
+
 num = ARGV[0].to_i
-n = num.abs
-f0 = 0
-f1 = 1
-if n==1 || n ==0
-  puts n
-  exit
-end
-for i in 2 .. n
-  fi = f0 + f1
-  f0 = f1
-  f1 = fi
-end
-if num < 0 && n % 2 == 0
-  fi = -fi
-end
-puts fi
+puts fibo(num)
