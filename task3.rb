c617fd27fffd7572f4b99b0ed5028a9eec1e7576
behuamuh <sven1980@list.ru>
2018-06-07 13:36:40 +0300

    Update task3.rb

--- task3.rb
+++ task3.rb
@@ -1,11 +1,10 @@
 # Task 3
-=begin
+
 str = ''
 for i in 0 ... ARGV.length
   str = str + ARGV[i]
 end
 d = str.scan(/\d/).join
-g = str.scan(/[aeiouyAEIOUYаеёиоуыэюяАЕЁИОУЫЭЮЯ]/).join
-s = str.scan(/[qwrtpsdfghjklzxcvbnmQWRTPSDFGHJKLZXCVBNMйцкнгшщзхъфвпрлджчсмтьбЙЦКНГШЩЗХЪФВПРЛДЖЧСМТЬБ]/).join
+g = str.scan(/[aeiouAEIOUаеёиоуыэюяАЕЁИОУЫЭЮЯ]/).join
+s = str.scan(/[qwrtpsdfghjklzxcvbnmyQWRTPSDFGHJKLZXCVBNMYйцкнгшщзхъфвпрлджчсмтьбЙЦКНГШЩЗХЪФВПРЛДЖЧСМТЬБ]/).join
 puts g + " " + s + " " + d
-=end
