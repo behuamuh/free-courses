239681dc5ebab4c4025bf56d3261862fe99c90a4
behuamuh <sven1980@list.ru>
2018-06-08 18:35:43 +0300

    Update task3.rb

--- task3.rb
+++ task3.rb
@@ -1,10 +1,14 @@
-# Task 3
-
 str = ''
 for i in 0 ... ARGV.length
   str = str + ARGV[i]
 end
 d = str.scan(/\d/).join
-g = str.scan(/[aeiouAEIOUаеёиоуыэюяАЕЁИОУЫЭЮЯ]/).join
+g = str.scan(/[aeiouyYAEIOUаеёиоуыэюяАЕЁИОУЫЭЮЯ]/).join
 s = str.scan(/[qwrtpsdfghjklzxcvbnmyQWRTPSDFGHJKLZXCVBNMYйцкнгшщзхъфвпрлджчсмтьбЙЦКНГШЩЗХЪФВПРЛДЖЧСМТЬБ]/).join
-puts g + " " + s + " " + d
+if g.length != 0
+	g += " "
+end
+if s.length != 0
+	s += " "
+end
+puts g + s + d
