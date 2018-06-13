4c08c19c7b909b1d9e33907b0883bb8a6cad6ebc
behuamuh <sven1980@list.ru>
2018-06-13 23:29:39 +0300

    Может так?

--- task2.rb
+++ task2.rb
@@ -1,9 +1,7 @@
-def getFibo(input)
-    def fib(n, hash = [0,1])
-        n.zero? ? hash[1] = 0 : hash.push(hash.reduce(:+)).shift
-        return n>2 ? fib(n-1, hash ) :  hash[1]
-    end
-    num = Integer(input)
-    return ( (num<0) && (num.abs%2).zero? ) ? -1*fib(num.abs) : fib(num.abs)
+require 'matrix'
+def fibo(input)
+  n = input.to_i
+  m = Matrix[[0, 1], [1,1]]
+  result = (m**(n-1))[1,1].to_i
 end
-puts getFibo(ARGV[0]).to_s
+puts fibo(ARGV[0])
