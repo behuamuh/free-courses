95512e15ba62494a8b96bcdeb0d79c6a512ad7a3
behuamuh <sven1980@list.ru>
2018-06-13 07:57:54 +0300

    Может так?

--- task2.rb
+++ task2.rb
@@ -1,8 +1,9 @@
-def getFibo(num)
+def getFibo(input)
     def fib(n, hash = [0,1])
         n.zero? ? hash[1] = 0 : hash.push(hash.reduce(:+)).shift
         return n>2 ? fib(n-1, hash ) :  hash[1]
     end
-    return ((num.to_i<0) && (num.to_i.abs%2).zero?) ? -1*fib(num.to_i.abs) : fib(num.to_i.abs)
+    num = Integer(input)
+    return ( (num<0) && (num.abs%2).zero? ) ? -1*fib(num.abs) : fib(num.abs)
 end
 puts getFibo(ARGV[0])
