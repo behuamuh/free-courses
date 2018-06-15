886eb0959f7dd5c85f7da002cc8606fe09496fec
behuamuh <sven1980@list.ru>
2018-06-15 12:40:27 +0300

    Облазил весь гугл. В этом алгоритме n*log n.

--- task2.rb
+++ task2.rb
@@ -1,7 +1,21 @@
-require 'matrix'
-def fibo(input)
-  n = input.to_i
-  m = Matrix[[0, 1], [1,1]]
-  result = (m**(n-1))[1,1].to_i
+def fast_fib(num)
+    num < 0 ? ((-1)**(num + 1)) * _fib(num.abs)[0] : _fib(num)[0]
 end
-puts fibo(ARGV[0])
+  
+def _fib(n)
+    if n == 0
+        return [0, 1]
+    else
+        n1, n2 = _fib(n / 2)
+        a = n1 * (n2 * 2 - n1)
+        b = n1 * n1 + n2 * n2
+
+        if n % 2 == 0
+            [a, b]
+        else
+            [b, a + b]
+        end
+    end
+end
+
+puts fast_fib(ARGV[0].to_i)
